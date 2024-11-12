91ee5a0ef22de0be9d9cee9a3f2f06e1f57fc362
Cascone <dcascone@deloitte.it>
2024-11-12 19:30:35 +0100

    server node configuration

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Servire i file statici dalla cartella di build di Quasar
+app.use(express.static(path.join(__dirname, 'dist/spa')));
+
+// Gestione delle richieste con history mode per le SPA
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/spa/index.html'));
+});
+
+// Avvia il server
+app.listen(port, () => {
+  console.log(`Server in ascolto sulla porta ${port}`);
+});
