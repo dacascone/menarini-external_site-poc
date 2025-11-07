2b98d56846f6904a0544538afab423dabd9f7e2d
dcascone <dcascone@deloitte.it>
2025-11-07 13:34:03 +0100

    consensi png

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,44 +1,32 @@
 <template>
-  <q-page className="flex flex-center">
-    <!-- Container for Preference Manager -->
-    <div>You are logged in. Please note that this is a <strong>demo site</strong></div>
-    <div id="pref-center-main"></div>
+  <q-page class="flex flex-center column q-pa-lg">
+    <!-- Immagine centrata -->
+    <img
+      src="~assets/consensi.png"
+      alt="Consensi"
+      class="consensi-image q-mb-md"
+    />
+
+    <!-- Testo demo -->
+    <div class="text-center">
+      You are logged in. Please note that this is a <strong>demo site</strong>.
+    </div>
+
+    <!-- Preference Center (se necessario) -->
+    <div id="pref-center-main" class="q-mt-lg"></div>
   </q-page>
 </template>
 
 <script setup>
-// import {onMounted} from 'vue'
-
 defineOptions({
   name: 'IndexPage'
 })
-
-/* onMounted(() => {
-  // Inject the Preference Manager CSS
-  const link = document.createElement('link')
-  link.rel = 'stylesheet'
-  link.type = 'text/css'
-  link.href = 'https://menarinipharma--developer.sandbox.my.salesforce.com/prefcenter/preference-center-1.2.0/css/pref-center-slds.css'
-  document.head.appendChild(link)
-
-  // Inject the Preference Manager Script
-  const script = document.createElement('script')
-  script.id = 'preference-center'
-  script.src = 'https://menarinipharma--developer.sandbox.my.salesforce.com/prefcenter/preference-center-1.2.0/js/main.js'
-  script.setAttribute('pref-center-host', 'https://menarinipharma--developer.sandbox.my.salesforce.com')
-  script.setAttribute('pref-center-name', 'Detailed_test')
-  script.setAttribute('pref-center-param', 'pctoken')
-  script.setAttribute('pctoken', 'QUtJMDAwMDAwY01qYWVUYUlGWHoxSExzZFVhUFhINHVuWnBwZ0o0bEliZ2dDWXlUbERoNXdDaFcxQ2FLZGQvdHAyMFRyWmU5M3g3U0lHODRBbUh0RFgrYW5RUHV3SHFCOE1MMURTQ1hnUUlEZ2VhQ2I4STdsV1VWZUhUZ3ZzNjMyS3huejdGN3hHbGpNUUdBWWpoYWNReStQOFdvaXFzNFQvdz09')
-  script.async = true
-
-  script.onload = () => {
-    console.log('Preference Manager script loaded successfully.')
-  }
-
-  script.onerror = () => {
-    console.error('Error loading the Preference Manager script.')
-  }
-
-  document.body.appendChild(script)
-}) */
 </script>
+
+<style scoped>
+.consensi-image {
+  max-width: 300px;
+  width: 100%;
+  height: auto;
+}
+</style>
