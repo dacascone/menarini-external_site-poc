73d0bd72f5e594397d2604056860a8b7b823be4d
dcascone <dcascone@deloitte.it>
2026-02-04 12:43:31 +0100

    fix

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -64,7 +64,7 @@ import { ref } from 'vue'
 
 // === CONFIG ===
 const SFDC_COMMUNITY_LOGOUT_URL =
-  'https://menarinipharma--test.sandbox.my.site.com/ciam/secur/logout.jsp'
+  'https://menarinipharma--test.sandbox.my.site.com/ciam/secur/logout.jsp/retUrl=https://aestheticsbymenarinithailand.com'
 
 // === MENU ACTIONS ===
 const openPreferenceCenter = () => {
